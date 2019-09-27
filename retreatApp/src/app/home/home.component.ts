a01b802fa1e9ba3eb2f798e3f1e82a9b9328433d
Guillermo Perez Ruiz <guillermo.perezruiz@kbc.be>
2019-09-27 22:12:20 +0200

    some variable added and applied them to home component

--- retreatApp/src/app/home/home.component.ts
+++ retreatApp/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  home_title = "WELCOME";
-  home_paragraph = "some paragraph";
+  home_title = 'WELCOME';
+  home_paragraph = 'some paragraph some paragraph some paragraph some paragraph some paragraph some paragraph';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
